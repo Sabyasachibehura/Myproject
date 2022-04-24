df368857023bbbb191a42c08f65b3b9a731666c1
Your Name <sabyasachibehura3@gmail.com>
2022-04-24 16:46:26 +0530

    added all files

---
+++ src/main/java/demo_app_5/DemoApp5Application.java
@@ -0,0 +1,13 @@
+package demo_app_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/demo_app_5/DemoApp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo_app_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
